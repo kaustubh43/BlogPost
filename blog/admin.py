2821378fca80d97c3776859bd0900657ae75446d
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-06-27 21:53:53 +0530

    configure-postgres and migrations

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    """To show limited fields in admin end point"""
+    list_display = ('title', 'created', 'author')
+
+admin.site.register(models.Blog, BlogAdmin)
