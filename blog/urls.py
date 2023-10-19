41215757bcf7d8334ffb1e8d38ca872bcc588dd6
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-10-19 19:54:24 +0530

    fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -10,9 +10,7 @@ urlpatterns = [
     path('blog/<int:pk>/edit', views.BlogUpdateView.as_view(), name='blog.edit'),
     path('blog/<int:pk>/delete', views.BlogDeleteView.as_view(), name='blog.delete'),
     path('blog/<int:pk>/accessdenied',views.AccessDenied.as_view(), name='blog.accessdenied'),
-    path('blog/<int:blog_id>/addcomment', views.BlogComment.as_view(), name='blog.addcomment')
-
-
+    path('blog/<int:blog_id>/addcomment', views.BlogComment.as_view(), name='blog.addcomment'),
 ]
 
 handler404 = 'blog.views.error_404_view'
